1e12d3ee3590255745dfa5605ecbf5dcb873fe88
joaohmorais <joao.tlp@gmail.com>
2019-09-01 20:22:03 -0300

    added instant make map function, easier dataframe retrieval

---
+++ R/tabnet_df.R
@@ -0,0 +1,47 @@
+#' Retrieve TABNET data in dataframe form
+#'
+#' Retrieve a dataframe with indicator data, per region, per year.
+#' @param indicator_index Indicator index number, @seealso [tabnet_index()].
+#' @param region String describing how should data be regionalized.
+#' @param subindicator Among the indicator, which subindicator should be used? Defaults to the last index.
+#' @param years Retrieve data from one or more specific years?
+#' @param onlyMostRecent Boolean. If true, will retrieve only data from the most recent year informed.
+#' @keywords tabnet
+#' @export
+#' @return Regionalized data frame from selected indicator
+#' @examples
+#' @seealso [tabnet_csv_retrieval()]
+
+tabnet_df <- function(indicator_index, region = "Município", subindicator = NULL, years = NULL, 
+                            onlyMostRecent = FALSE) {
+  matriz <- tabnet_index()
+  data <- NULL
+  if (indicator_index %in% c(1:length(matriz$Links))) {
+    obj <- make_tabnet_obj(matriz$Links[indicator_index])
+    linha <- min(which(obj$NomesLinhas == region))
+    indicator <- ifelse(is.null(subindicator), length(obj$Indicadores), subindicator)
+    anos <- obj$Arquivos
+    if (!is.null(years)) {
+      anos <- obj$Arquivos[obj$Anos %in% years]
+    }
+    
+    if (onlyMostRecent) {
+      anos <- anos[1]
+    }
+    
+    if (linha == 0) {
+      warning("region not found")
+    } else if (indicator > length(obj$Indicadores)) {
+      warning("indicator not found")
+    } else {
+      print(paste0("Returning indicator: '", obj$NomesIndicadores[indicator],
+                   "'; To retrieve a different one, use 'subindicator' index."))
+      body <- getPostRequestBody(obj$Linhas[linha], obj$Indicadores[indicator], anos)
+      data <- tabnet_csv_retrieval(obj$POST_url, body, region)
+    }
+  } else {
+    warning("indicator index not found among available indicators")
+  }
+  
+  return(data)
+}
---
+++ R/tabnet_map.R
@@ -0,0 +1,75 @@
+library(dplyr)
+library(ggplot2)
+
+#' Generate Map plot from indicator data
+#'
+#' Generate a ggplot2 map from indicator data of a specific year.
+#' @param indicator_index Indicator index number, @seealso [tabnet_index()].
+#' @param region String describing how should data be regionalized.
+#' @param subindicator Among the indicator, which subindicator should be used? Defaults to the last index.
+#' @param years Retrieve data from one or more specific years?
+#' @param label Boolean. Should the regions be labeled? Defaults to false.
+#' @param nBins Number of bins to discretize desired indicator value.
+#' @param palette RColorBrewer palette.
+#' @param title Do you want to provide a specific title? Otherwise, function will generate one itself.
+#' @keywords tabnet
+#' @return ggplot2 object
+#' @export
+#' @examples
+
+tabnet_map <- function(indicator_index, region = "Município", subindicator = NULL, years = NULL, 
+                       label = FALSE, nBins = NULL, palette = "Purples", title = NULL) {
+  
+  #ShapeFiles
+  sp_cities <- readRDS("../shapefile/simpl_sp.rds")
+  sp_rras <- readRDS("../shapefile/simpl_rras.rds")
+  sp_drs <- readRDS("../shapefile/simpl_drs.rds")
+  sp_reg_saude <- readRDS("../shapefile/simpl_reg_saude.rds")
+  
+  
+  data <- tabnet_df(indicator_index, region, subindicator, years, onlyMostRecent = TRUE)
+  obj <- make_tabnet_obj(tabnet_index()$Links[indicator_index])
+  indicator <- ifelse(is.null(subindicator), length(obj$Indicadores), subindicator)
+  geometry <- switch (region,
+                      "Município" = sp_cities,
+                      "DRS" = sp_drs,
+                      "RRAS" = sp_rras,
+                      "Região de Saúde" = sp_reg_saude
+  )
+  
+  key <- switch (region,
+                 "Município" = "cod_mun",
+                 "DRS" = "cod_drs",
+                 "RRAS" = "cod_rras",
+                 "Região de Saúde" = "cod_rs")
+  
+  colnames(data)[c(1, 2)] <- c(key, "Region")
+  plotData <- merge(geometry, data, by=key)
+  g <- NULL
+  if (!is.null(nBins)) {
+    plotData <- plotData %>% 
+      mutate(valor_discreto = cut_number(Valor, nBins))
+    
+    g <- ggplot(data=plotData) + 
+      geom_sf(aes(fill=valor_discreto)) + 
+      scale_fill_brewer(obj$NomesIndicadores[indicator], palette = palette)
+  } else {
+    g <- ggplot(data=plotData) + 
+      geom_sf(aes(fill=Valor)) +  
+      scale_fill_distiller(obj$NomesIndicadores[indicator], palette = palette)
+  }
+  
+  g <- g + xlab("Longitude") + ylab("Latitude") + 
+    ggtitle(ifelse(is.null(title), paste0(obj$NomesIndicadores[indicator], ", por ", 
+                                          region, ", em ", levels(plotData$Ano)), 
+                   title)) +
+    theme(panel.grid.major = element_blank(), panel.background = element_rect(fill = "aliceblue"), 
+          title = element_text(hjust = 0.5, size = 12), axis.title.x = element_text(size = 8), 
+          axis.title.y = element_text(size = 8)) 
+  
+  if (label) {
+    g <- g + geom_sf_text(aes(label = Region), size = 2)
+  }
+  
+  g
+}
