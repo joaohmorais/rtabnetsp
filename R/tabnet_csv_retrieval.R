71a43c816d5aa7beef06d8003c7544209ac7ff7f
joaohmorais <joao.tlp@gmail.com>
2019-09-17 19:55:04 -0300

    added timeout

--- R/tabnet_csv_retrieval.R
+++ R/tabnet_csv_retrieval.R
@@ -17,41 +17,49 @@ library(tidyr)
 #' @examples
 tabnet_csv_retrieval <- function(post_url, body, colname = "Região", ind_name = "Valor", skip = 3, stopDelta = 6) {
   base_url <- "http://tabnet.saude.sp.gov.br"
-  response <- POST(post_url,body = body, encode="raw")
-  response_text <- content(response, as="text", encoding = "latin1")
-  start <- regexpr("/csv", response_text)[1]
-  end <- regexpr("csv>", response_text)[1] + 2
-  path <- substr(response_text, start, end)
-  stopPoint <- min(which(grepl("Fonte", readLines(paste0(base_url, path), encoding = "latin1")) == TRUE))
-  
-  
-  #data <- read.csv(paste0(base_url, path), skip = skip, sep=";", nrow = stopPoint - stopDelta, encoding = "latin1")
-  download.file(paste0(base_url, path), "indicator_data.csv")
-  data <- read.csv("indicator_data.csv", skip = skip, sep=";", nrow = stopPoint - stopDelta, encoding = "latin1")
-  colnames(data) <- c(colnames(data)[1], gsub("X", "", colnames(data)[-1]))
-  #formatting
-  
-  #names
-  data[,1] <- as.character(data[,1])
-  
-  #numbers
-  for (i in c(2:ncol(data))) {
-    data[,i] <- as.numeric(gsub(",", ".", gsub("\\.", "", data[,i])))
+  data <- NULL
+  response <- NULL
+  attempt <- 1
+  while (is.null(response) && attemp <= 3) {
+    response <- POST(post_url,body = body, encode="raw", config = timeout(0.2))
+    attempt <- attempt + 1
   }
-
-  #ids
-  
-  div_index <- unlist(gregexpr("[0-9] ", data[,1]))
-  data$id <- as.numeric(substr(data[,1], 1, div_index))
-  data[,1] <- substr(data[,1], div_index + 2, nchar(data[,1]))
-  data <- data[,c(ncol(data), 1:ncol(data)-1)]
-  
-  #remove total
-  data <- data[,c(1:ncol(data)-1)]
-
-  data <- data %>% gather(Ano, Valor, 3:ncol(data))
-  colnames(data)[colnames(data) == "Valor"] <- ind_name
-  
-  data$Ano <- as.factor(data$Ano)
+  if (!is.null(response)) {
+    response_text <- content(response, as="text", encoding = "latin1")
+    start <- regexpr("/csv", response_text)[1]
+    end <- regexpr("csv>", response_text)[1] + 2
+    path <- substr(response_text, start, end)
+    stopPoint <- min(which(grepl("Fonte", readLines(paste0(base_url, path), encoding = "latin1")) == TRUE))
+    download.file(paste0(base_url, path), "indicator_data.csv")
+    data <- read.csv("indicator_data.csv", skip = skip, sep=";", nrow = stopPoint - stopDelta, encoding = "latin1")
+    colnames(data) <- c(colnames(data)[1], gsub("X", "", colnames(data)[-1]))
+    #formatting
+    
+    #names
+    data[,1] <- as.character(data[,1])
+    
+    #numbers
+    for (i in c(2:ncol(data))) {
+      data[,i] <- as.numeric(gsub(",", ".", gsub("\\.", "", data[,i])))
+    }
+    
+    #ids
+    
+    div_index <- unlist(gregexpr("[0-9] ", data[,1]))
+    data$id <- as.numeric(substr(data[,1], 1, div_index))
+    data[,1] <- substr(data[,1], div_index + 2, nchar(data[,1]))
+    data <- data[,c(ncol(data), 1:ncol(data)-1)]
+    
+    #remove total
+    data <- data[,c(1:ncol(data)-1)]
+    
+    data <- data %>% gather(Ano, Valor, 3:ncol(data))
+    colnames(data)[colnames(data) == "Valor"] <- ind_name
+    
+    data$Ano <- as.factor(data$Ano)
+    
+  } else {
+    warning("Could not connect to TabNet: Connection timed out several times.")
+  }
   return(data)
 }
--- R/tabnet_df.R
+++ R/tabnet_df.R
@@ -36,16 +36,9 @@ tabnet_df <- function(indicator_index, region = "Município", subindicator = NUL
     } else {
       print(paste0("Returning indicator: '", obj$NomesIndicadores[indicator],
                    "'; To retrieve a different one, use 'subindicator' index."))
-      print(paste0("linha:", obj$Linhas[linha]))
-      print(obj$Indicadores[indicator])
-      print(anos)
       
       body <- getPostRequestBody(obj$Linhas[linha], obj$Indicadores[indicator], anos)
-      print(body)
-      print(obj$POST_url)
-      print(region)
       data <- tabnet_csv_retrieval(obj$POST_url, body, region)
-      print(head(data))
     }
   } else {
     warning("indicator index not found among available indicators")
